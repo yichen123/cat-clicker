77520a3920f4cc0ce70253099d1a9eefd2635bde
yichen123 <chenyi330@hotmail.com>
2015-07-10 21:38:45 +0800

    requriment3 is done

--- js/main.js
+++ js/main.js
@@ -1,16 +1,55 @@
-var cat1Name = document.getElementById("cat1Name");
-cat1Name.innerText = "Ross";
-var cat2Name = document.getElementById("cat2Name");
-cat2Name.innerText = "Bob";
 
-var counting = function(){
-  var counterTag = document.getElementById("counter");
-  counterTag.innerText = parseInt(counterTag.innerText) + 1;
-  console.log(counterTag.innerText);
-};
+var catNames = ["Bob", "Ross", "Eva"];
 
-var pic1 = document.getElementById("mycat1");
-pic1.addEventListener('click', counting)
+var increment = function(astring){
+    console.log(astring);
+}
 
-var pic2 = document.getElementById("mycat2");
-pic2.addEventListener('click', counting)
+var displayBox = document.getElementsByClassName("row");
+for (var i = 0; i < catNames.length; i += 1) {
+    var catContainer,
+        imageContainer,
+        image,
+        informationContainer,
+        nameContainer, //box for placing cat name
+        catName,
+        counterContainer, //box for placing counter
+        counter;
+
+    catContainer = document.createElement('div');
+    catContainer.classList.add("catContainer");
+
+    imageContainer = document.createElement('div');
+    imageContainer.classList.add("imageContainer");
+    image = document.createElement('img');
+    var p = i + 1;
+    image.src = "img/cat" + p + ".jpg";
+    imageContainer.appendChild(image);
+    catContainer.appendChild(imageContainer);
+
+    informationContainer = document.createElement('div');
+    informationContainer.classList.add("informationContainer");
+
+    nameContainer = document.createElement('div');
+    nameContainer.classList.add("nameContainer");
+    catName = document.createElement('h2');
+    catName.innerText = catNames[i];
+    nameContainer.appendChild(catName);
+    informationContainer.appendChild(nameContainer);
+
+    counterContainer = document.createElement('div');
+    counterContainer.classList.add("counterContainer");
+    counter = document.createElement('h4');
+    counter.innerText = 0;
+    counterContainer.appendChild(counter);
+    informationContainer.appendChild(counterContainer);
+    catContainer.appendChild(informationContainer);
+
+    displayBox[0].appendChild(catContainer);
+
+    image.addEventListener('click', (function(number) {
+        return function() {
+            number.innerText = parseInt(number.innerText) + 1;
+        }
+    })(counter));
+}
