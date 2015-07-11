c7cd149790f2c5cc46398b8a28c6a87ae55ad7ba
yichen123 <chenyi330@hotmail.com>
2015-07-11 21:10:44 +0800

    reconstruct the js file, and finished model section

---
+++ js/app.js
@@ -0,0 +1,21 @@
+model = [{
+    'name': 'Bob',
+    'counter': 0,
+    'img': 'img/cat1.jpg'
+}, {
+    'name': 'Jack',
+    'counter': 0,
+    'img': 'img/cat2.jpg'
+}, {
+    'name': 'Ross',
+    'counter': 0,
+    'img': 'img/cat3.jpg'
+}, {
+    'name': 'Marry',
+    'counter': 0,
+    'img': 'img/cat4.jpg'
+}, {
+    'name': 'Lora',
+    'counter': 0,
+    'img': 'img/cat5.jpg'
+}];
